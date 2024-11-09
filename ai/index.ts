ca6bd147c811936632dbec4ed941e275ae3a200a
Dias Karshal <87409415+dkarshal@users.noreply.github.com>
2024-11-09 22:09:02 +0500

    openai --> gemini

--- ai/index.ts
+++ ai/index.ts
@@ -1,11 +1,11 @@
-import { openai } from '@ai-sdk/openai';
+import { openai } from '@ai-sdk/gemini';
 import { experimental_wrapLanguageModel as wrapLanguageModel } from 'ai';
 
 import { customMiddleware } from './custom-middleware';
 
 export const customModel = (apiIdentifier: string) => {
   return wrapLanguageModel({
-    model: openai(apiIdentifier),
+    model: gemini(apiIdentifier),
     middleware: customMiddleware,
   });
 };
